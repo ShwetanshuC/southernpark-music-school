0fbc1bd7b1fc33b8bd5c8441070c0430901da250
Shwetanshu <shwetanshu.c@gmail.com>
2026-04-04 20:26:21 -0400

    Temp: expose WSGI startup error in HTTP response for debugging

--- southernpark/wsgi.py
+++ southernpark/wsgi.py
@@ -1,5 +1,13 @@
 import os
+import traceback
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "southernpark.settings")
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    _error = traceback.format_exc().encode()
+    def application(environ, start_response):
+        start_response("500 Internal Server Error", [("Content-Type", "text/plain")])
+        return [_error]
