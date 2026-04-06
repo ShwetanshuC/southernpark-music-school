c06613e09da6791c42f1f6f776f9d4bdbb2cec2b
Shwetanshu <shwetanshu.c@gmail.com>
2026-04-06 13:25:20 -0400

    Fix 500 reset by removing restore_db from wsgi.py

--- southernpark/wsgi.py
+++ southernpark/wsgi.py
@@ -4,11 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "southernpark.settings")
 application = get_wsgi_application()
 
-try:
-    # Check if we're dealing with sqlite and should sync
-    # We only do this if DATABASE_URL is not set (meaning we are using local sqlite)
-    if not os.environ.get("DATABASE_URL") and not os.environ.get("DB_NAME"):
-        from sitecontent.s3_backup import restore_db
-        restore_db()
-except ImportError:
-    pass
